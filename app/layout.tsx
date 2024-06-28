c05561272d17809b39431a7b26887187729d8173
amurrai <andre.murrai@gmail.com>
2024-06-28 17:24:09 -0400

    Configure theme settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,46 @@
 import '@mantine/core/styles.css';
 
-import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import { ColorSchemeScript, MantineProvider, createTheme } from '@mantine/core';
 
 export const metadata = {
   title: 'ZeeK',
   description: 'Sweet Geek',
 };
+
+const theme = createTheme({
+
+  primaryColor: 'orange',
+  colors: {
+    orange: [
+      "#fff0e3",
+      "#ffdfcd",
+      "#ffbe9b",
+      "#ff9b64",
+      "#fe7c37",
+      "#fe6a1a",
+      "#ff5f09",
+      "#e44f00",
+      "#cb4500",
+      "#b13900"
+    ],
+    brown: [
+      "#fff2eb",
+      "#fae1d4",
+      "#f9c1a3",
+      "#fa9e6c",
+      "#fa8140",
+      "#fa6d25",
+      "#fb6418",
+      "#df540e",
+      "#c74908",
+      "#ad3d00"
+    ]
+  },
+  fontFamily: 'Cutive',
+  headings: {
+    fontFamily: 'Urbanist'
+  }
+})
 
 export default function RootLayout({
   children,
@@ -16,9 +51,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <ColorSchemeScript />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Cutive&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider theme={theme} withGlobalStyles>{children}</MantineProvider>
       </body>
     </html>
   );
---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { AppShell, Burger, Button, Group, Skeleton, Title } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+export default function Home() {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Title>Zeek Confections</Title>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>Main
+        <Button variant='filled'>Test</Button>
+      </AppShell.Main>
+    </AppShell>
+  );
+}
