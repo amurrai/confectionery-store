7a05645bda5defbf958ac0ca11a5f958c73938f0
amurrai <andre.murrai@gmail.com>
2024-07-01 18:50:03 -0400

    Format Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,63 +1,25 @@
 "use client"
+import { Fieldset, TextInput, Textarea, Button, Container, Title, List } from '@mantine/core';
+import React from 'react';
 
-import React, { useState } from 'react';
-import { TextInput, Textarea, Button, Container, Title } from '@mantine/core';
-
-const ContactPage: React.FC = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
-  };
-
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
-
-  const handleMessageChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMessage(event.target.value);
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Handle form submission logic here
-  };
-
+export default function ContactPage() {
   return (
-    <Container>
+    <Container className='flex flex-col w-1/2 mt-20 min-w-max'>
       <Title>Contact Us</Title>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <TextInput id="name" value={name} onChange={handleNameChange} />
-        </div>
-        <div>
-          <TextInput label="Email:" id="email" type="email" value={email} onChange={handleEmailChange} />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <Textarea id="message" value={message} onChange={handleMessageChange} />
-        </div>
-        <Button type="submit">Submit</Button>
-      </form>
-      <div>
-        <Title order={2}>Follow us on social media:</Title >
-        <ul>
-          <li>
-            <a href="https://www.facebook.com">Facebook</a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com">Twitter</a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com">Instagram</a>
-          </li>
-        </ul>
-      </div>
+      <Fieldset variant="unstyled">
+        <TextInput label="Name" />
+        <TextInput label="Email" />
+        <Textarea label="Message" />
+        <Button type="submit" className='my-5'>Submit</Button>
+      </Fieldset>
+      <Container>
+        <Title order={2}>Follow us on social media</Title >
+        <List>
+          <List.Item>Instagram</List.Item>
+          <List.Item>Facebook</List.Item>
+          <List.Item>Twitter</List.Item>
+        </List>
+      </Container>
     </Container>
   );
 };
-
-export default ContactPage;
